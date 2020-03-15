b43def88520c2b1f9e0c03e89aabac5d4784e9aa
ggboy-shakalaka <switch2018@outlook.com>
2020-03-15 20:40:33 +0800

    3.0

--- sso-web/src/main/java/cn/zhaizq/sso/web/controller/abc/ApiController.java
+++ sso-web/src/main/java/cn/zhaizq/sso/web/controller/abc/ApiController.java
@@ -45,24 +45,9 @@ public class ApiController extends BaseController {
     public SsoResponse<SsoConfig.ServerConfig> query_config(@PathVariable String appId) {
         SsoConfig.ServerConfig config = new SsoConfig.ServerConfig();
         config.setServerUrl(ssoConfig.getServerConfig().getServerUrl());
-        config.setLoginPath(String.format("/api/%s/login", appId));
-        config.setLogoutPath(String.format("/api/%s/logout", appId));
-        config.setCheckTokenPath(String.format("/api/%s/check_token", appId));
         config.setRefreshTokenPath(String.format("/api/%s/refresh_token", appId));
 
         return new SsoResponse<SsoConfig.ServerConfig>().code(200).data(config);
-    }
-
-    @PostMapping("/check_token")
-    public SsoResponse<SsoUser> check_token(@PathVariable String appId, @RequestBody SsoCheckTokenRequest ssoCheckToken) {
-        User user = userCache.get(ssoCheckToken.getToken());
-        if (user == null)
-            return new SsoResponse<SsoUser>().code(400);
-
-        SsoUser ssoUser = new SsoUser();
-        ssoUser.setId(user.getId() + "");
-        ssoUser.setName(user.getUserName());
-        return new SsoResponse<SsoUser>().code(200).data(ssoUser);
     }
 
     @GetMapping("/refresh_token")
@@ -85,36 +70,5 @@ public class ApiController extends BaseController {
         uri.addParameter(SsoConstant.REDIRECT, redirect);
         uri.addParameter("appId", appId);
         response.sendRedirect(uri.toString());
-    }
-
-    @PostMapping("/login")
-    @ResponseBody
-    public SsoResponse<String> login(@PathVariable String appId, @RequestBody SsoLoginRequest loginRequest) throws BadPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, IOException, NoSuchPaddingException, InvalidKeyException, InvalidKeySpecException {
-        User user = loginService.login(loginRequest.getName(), loginRequest.getPassword());
-
-        if (user == null) {
-            throw new BusinessException("用户名或密码错误");
-        }
-
-        String ssoToken = tokenCache.get(loginRequest.getName());
-        if (ssoToken == null) {
-            tokenCache.put(loginRequest.getName(), ssoToken = UUID.randomUUID().toString());
-        }
-
-        userCache.put(ssoToken, user);
-
-        return new SsoResponse<String>().code(200).data(ssoToken);
-    }
-
-    @PostMapping("/logout")
-    public SsoResponse<?> logout(@RequestBody SsoLogoutRequest request) {
-        userCache.put(request.getToken(), null);
-        return new SsoResponse<>().code(200);
-    }
-
-    @GetMapping("/getPublicKey")
-    public SsoResponse<String> getPublicKey(@RequestParam String name) throws NoSuchAlgorithmException {
-        String publicKey = loginService.getPublicKeyByName(name);
-        return new SsoResponse<String>().code(200).data(publicKey);
     }
 }
